b92aa78c3dd9ca8e61d60f6987eeccffa84375a1
Kelly Jiang <kllyjiang@gmail.com>
2019-10-04 23:03:49 -0500

    Using dotenv to hide api key

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
-console.log('this is loaded');
-
 exports.spotify = {
-  id: "719cd359621942bab892df3172d26e38",
-  secret: "2d4d0e4e77d24cda9ac92d49ebcd893a"
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
